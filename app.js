2ce970a225c518f9ae93a63bbca1ff8b5a35397b
Alain <alan15lvs@gmail.com>
2019-05-04 13:29:04 -0300

    Simplificando componentes de React JSX

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ let hola = class Hola extends React.Component {
 
 	render(){
 		//React.createElement(ETIQUETA, ATRIBUTOS, CONTENIDO, PROPIEDADES DEL OBJETO)
-		let etiqueta = React.createElement("div", null, "Hola ", this.props.nombre)
-		return etiqueta
+		// let etiqueta = React.createElement("div", null, "Hola ", this.props.nombre)
+		return React.createElement("div", null, "Hola ", this.props.nombre)
+		// return etiqueta
 	}
 }
 ///////////////////////////////////////////////////////////////////
-// ReactDOM.render(COMPONENTE, AREA)
-let app = document.querySelector("#app")
-let saludo = React.createElement(hola, {nombre: "React !!!!"})
+/////// ReactDOM.render(COMPONENTE, AREA)
+// let app = document.querySelector("#app")
+// let saludo = React.createElement(hola, {nombre: "React !!!!"})
 
-ReactDOM.render(saludo, app)
+// ReactDOM.render(saludo, app)
+
+ReactDOM.render(
+	React.createElement(hola, {nombre:"React...."}),
+	document.querySelector("#app")
+)
