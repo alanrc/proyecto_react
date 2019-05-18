1b1753fb3857d7181c961612a66d28f4c1a3813f
Alain <alan15lvs@gmail.com>
2019-05-18 11:06:44 -0300

    refactorizando

---
+++ Componentes-React/app.js
@@ -0,0 +1,24 @@
+let Hola = class Hola extends React.Component {
+	render(){
+    	//React.createElement(ETIQUETA, ATRIBUTOS, CONTENIDO, PROPIEDADES DEL OBJETO)
+    	return React.createElement(
+			"div", //<--  Etiqueta
+			{ style: {color: 'red', fontFamily: 'Arial'} }, //<-- Atributos
+			"Hola ", //<-- Contenido estatico
+			this.props.nombre  //<-- Contenido dinamico (Propiedades del objeto)
+		)
+	}
+}
+
+let Imagen =  class Imagen extends React.Component {
+	render() {
+		return React.createElement("img", { src : this.props.url }, null)
+	}
+}
+///////////////////////////////////////
+//ReactDOM.render(COMPONENTE, AREA)
+ReactDOM.render(
+	// React.createElement(Hola, { nombre : "Luna" }),
+	React.createElement(Imagen, { url : "https://lorempixel.com/100/100/animals" }),
+	document.querySelector("#app")
+)
