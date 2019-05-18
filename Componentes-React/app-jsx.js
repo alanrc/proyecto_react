85158435fb226c22ee84b78b75d0b79685fba415
Alain <alan15lvs@gmail.com>
2019-05-18 20:41:43 -0300

    Componentes con JSX y manejo de estados

--- Componentes-React/app-jsx.js
+++ Componentes-React/app-jsx.js
@@ -1,4 +1,16 @@
+/* Componentes */
+class Hola extends React.Component {
+
+	render(){
+		return <div>Hola {this.props.nombre}!!!</div>
+	}
+  
+  }
+  
+////////////////////////////////////////////////////////
+/* Visualizacion de componentes */
+
 ReactDOM.render(
-  <Hola nombre="Modulo 'Hola' en JSX" />,
-  document.querySelector("#app")
+	<Hola nombre="Modulo 'Hola' en JSX" />,
+	document.querySelector("#app")
 )
