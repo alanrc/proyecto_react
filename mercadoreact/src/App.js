5877a94402ec85a370320eeac37ce0f3a3982e8a
Alain <alan15lvs@gmail.com>
2019-06-08 14:42:12 -0300

    Implementando datos con JSON + Helpers + Eventos

--- mercadoreact/src/App.js
+++ mercadoreact/src/App.js
@@ -3,31 +3,48 @@ import React from 'react';
 /* Importar modulos propios */
 import Header from './components/Header'
 import Menu from './components/Menu'
+import Product from './components/Product'
+/* Impoertar recursos propios */
 // import './App.css';
+import productos from './productos.json'
+
 
 class App extends React.Component {
 
 	constructor() {
 		super()
+		console.log(productos)
 		this.state = {
 			title : "MercadoReact",
 			slogan : "Múcho más que JavaScript",
 			// products : ["Música", "Deportes", "Electro", "Pesca", "Calzado Masculino"]
+			/*
 			products : [
 				{ nombre : "Guitarra Criolla", precio : 1200, stock : 300, categoria : "Música" },
 				{ nombre : "Paleta Pin Pong x 2", precio : 350, stock : 650, categoria : "Deportes" },
 				{ nombre : "Microondas", precio : 8600, stock : 100, categoria : "Electro" },
-				{ nombre : "Pelota Basquet", precio : 960, stock : 225, categoria : "Deportes" }
-			]
+				{ nombre : "Pelota Basquet", precio : 960, stock : 225, categoria : "Deportes" },
+				{ nombre : "Marcadores de Color", precio : 85, stock : 400, categoria : "Librería" }
+			] */
+			products : productos
 		}
 	}
 
 	render() {
+		/* Acá se pueden programar cosas que desemboquen en el return */
+		// const losProductos = <Product item={this.state.products[67]} />
+		const losProductos = this.state.products.map(
+			(prodcut , index) => <Product item={prodcut} key={index} />
+		)
 		return (
 			/***************  ACÁ VAN TODOS LOS COMPONENTES   *************/   
 			<div className="App">
 				<Header title={this.state.title} slogan={this.state.slogan} />
 				<Menu links={this.state.products} />
+				{/* <Product item={this.state.products[53]} /> */}
+				<section className="row">
+					{losProductos}
+				</section>
 				
 			</div>
 			/*****************    ACÁ FUERA NADA   *************/
--- mercadoreact/src/components/Menu.js
+++ mercadoreact/src/components/Menu.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import slugify from '../helpers/String'
 
 // class Menu extends React.Component
 class Menu extends Component {
@@ -19,16 +20,23 @@ class Items extends Component {
 	constructor() {
 		super()
 		this.state = {
-			categories : null
+			categories : [],
+			loaded : false
 		}
 	}
 
 	componentDidMount() {
+
+		console.log("estos son links abajo " )
+		console.log(...this.props.links)
+		
 		this.setState({
-			categories : new Set( this.props.links.map(link => link.categoria) )
+			categories : [ ...new Set( this.props.links.map(link => link.Categoria) ) ],
+			loaded : true
 		})
 	}
 
+	/*
 	slugify(string) { // -----   Convertir texto a URL Friendly   ------
 		const a = 'àáäâãåăæçèéëêǵḧìíïîḿńǹñòóöôœøṕŕßśșțùúüûǘẃẍÿź·/_,:;'
 		const b = 'aaaaaaaaceeeeghiiiimnnnooooooprssstuuuuuwxyz------'
@@ -42,18 +50,23 @@ class Items extends Component {
 			.replace(/--+/g, '-') // Replace multiple - with single -
 			.replace(/^-+/, '') // Trim - from start of text
 			.replace(/-+$/, '') // Trim - from end of text
-	}
+	} */
 
 	render() {
 		
 		console.log(this.state.categories)
 
-		return this.props.links.map(
-			(link, index) =>
-			<li className="nav-item" key={index}>
-				<a className="nav-link" href={this.slugify(link.categoria)}>{link.categoria}</a>
-			</li>
-		)
+		if (!this.state.loaded) {
+			return <li>Cargando..</li>
+		} else {
+			console.log("Hay datos ... el estado esta cargado :)")
+			return this.state.categories.map(
+				(link, index) =>
+				<li className="nav-item" key={index}>
+					<a className="nav-link" href={slugify(link)}>{link}</a>
+				</li>
+			)
+		}
 	}
 }
 
