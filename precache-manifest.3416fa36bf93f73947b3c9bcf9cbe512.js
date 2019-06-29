42e2bc73755d0a76d5e696ef0ba39625d8265faa
Alain <alan15lvs@gmail.com>
2019-06-29 14:29:25 -0300

    Updates

---
+++ precache-manifest.3416fa36bf93f73947b3c9bcf9cbe512.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbdf2a2c547dd805c01d8b9a78c06fa2",
+    "url": "/alanrc/proyecto_react/index.html"
+  },
+  {
+    "revision": "d8719d7766816ab86f4f",
+    "url": "/alanrc/proyecto_react/static/js/2.2257cac7.chunk.js"
+  },
+  {
+    "revision": "bc5a34f1633144996a90",
+    "url": "/alanrc/proyecto_react/static/js/main.d409316d.chunk.js"
+  },
+  {
+    "revision": "2167b5257f118a9bdaa0",
+    "url": "/alanrc/proyecto_react/static/js/runtime~main.ba110016.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/alanrc/proyecto_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
